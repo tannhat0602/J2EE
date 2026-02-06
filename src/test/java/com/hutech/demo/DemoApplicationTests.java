cc367b273ca15159597b87257076f01ea81dd609
Phung Nhat Tan <phungnhattan060204@gmail.com>
2026-02-06 08:40:02 +0700

    fix

--- src/test/java/com/hutech/demo/DemoApplicationTests.java
+++ src/test/java/com/hutech/demo/DemoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.hutech.demo;
 
+import com.hutech.phuc.DemoApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
 	@Test
